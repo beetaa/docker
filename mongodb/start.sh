06c7f24d022f608e64cd7db4e16d2f105d309cb2
beetaa <beetaa@outlook.com>
2015-03-05 07:19:16 +0800

    update

---
+++ mongodb/start.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# @Author: anchen
+# @Date:   2015-03-04 15:46:52
+# @Last Modified by:   anchen
+# @Last Modified time: 2015-03-04 16:23:42
+# 
+mongod --dbpath /data/mongodb/ --logpath /data/mongodb/mongodb.log --logappend --rest --httpinterface
